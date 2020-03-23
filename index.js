cec884feb775f735fbc065447e2cdd9a5b2619b1
Ivandi Santoso <ivandisantoso@Vahns-MacBook-Pro.local>
2020-03-23 15:09:21 -0400

    Web socket implemented

--- index.js
+++ index.js
@@ -7,19 +7,31 @@ const path = require('path')
 var app = express();
 
 //Setup server
-var server = app.listen(PORT,()=>{
+var server = app.listen(PORT,function(){
   console.log(`Web server is up and running on port ${PORT}!`)
 })
 
 
 //Static files
-// app.use(express.static('public'));
-app.use('/', express.static(path.join(__dirname, '/public')))
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+app.use(express.static('public'));
+// app.use('/', express.static(path.join(__dirname, '/public')));
+// app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 //Socket setup
 var io = socket(server);
 
 io.on('connection',function(socket){
   console.log(`a user connected`,socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+      // console.log(data);
+      io.sockets.emit('chat', data);
+  });
+
+  // Handle typing event
+  socket.on('typing', function(data){
+      socket.broadcast.emit('typing', data);
+  });
+
 });
