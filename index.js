0b17a61a29cb745662442a7d55b362f0b06e9f37
Ivandi Santoso <ivandisantoso@Vahns-MacBook-Pro.local>
2020-03-23 14:23:52 -0400

    path added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const PORT = process.env.PORT || 3000;
 var socket = require('socket.io');
+const path = require('path')
 
 //App Setup
 var app = express();
@@ -12,8 +13,9 @@ var server = app.listen(PORT,()=>{
 
 
 //Static files
-app.use(express.static('public'));
-
+// app.use(express.static('public'));
+app.use('/', express.static(path.join(__dirname, '/public')))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
 
 //Socket setup
 var io = socket(server);
